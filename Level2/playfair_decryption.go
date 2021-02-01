d6dd1fd6dbe9fc78ee086d2955581f4f72001468
Yatharth Goswami <yatharthgoswami15@gmail.com>
2021-02-02 00:55:46 +0530

    Add bigram generator

--- Level2/playfair_decryption.go
+++ Level2/playfair_decryption.go
@@ -1,12 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
-	var playfair_matrix = [5][5]string{{"S","E","C","U","R"}, 
+	playfair_matrix := [5][5]string{{"S","E","C","U","R"}, 
 										{"I","T","Y","A","B"},
 										{"D","F","G","H","K"},
 										{"L","M","N","O","P"},
 										{"Q","V","W","X","Z"}}
-	fmt.Println(playfair_matrix[3][4])
+	_ = playfair_matrix
+	cipher_text := "B MH AFC MUVY EOHPTCS, AFCSS TE QCSI NTYIMS TNA AFCSC. EMRBH XAA VAFR MIUCQPUH LMRLCCETOT FN HM AKUXAHK. OTA WANA OTXT FFU EISCWNAF HME BFU MCVA UGTOTRE. BM HYLF IFU UVTY ANE HBSEI QYOQM OUVSF AM EAFTE PYHYS XNSKE IFUSC."
+	//key := "SECURITY"
+	bigrams := []string{}
+	cipher_text = strings.ReplaceAll(cipher_text, " ", "")
+	cipher_text = strings.ReplaceAll(cipher_text, ".", "")
+	for i := 0; i < len(cipher_text); i += 2 {
+		temp := ""
+		if (i < len(cipher_text)-1) {
+			temp = string(cipher_text[i]) + string(cipher_text[i+1])
+		} else {
+			temp = string(cipher_text[i]) + "z"
+		}
+		bigrams = append(bigrams, temp)
+	}
+	fmt.Println(cipher_text)
+	fmt.Println(bigrams)
 }
