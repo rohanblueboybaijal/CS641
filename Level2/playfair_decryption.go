42ef6d25e3ad81d2ede19222b8d4153de5cc492e
Yatharth Goswami <yatharthgoswami15@gmail.com>
2021-02-02 01:22:56 +0530

    Add mapping in Level2

--- Level2/playfair_decryption.go
+++ Level2/playfair_decryption.go
@@ -5,18 +5,24 @@ import (
 	"strings"
 )
 
+type loc struct {
+	x int
+	y int 
+}
+
 func main() {
 	playfair_matrix := [5][5]string{{"S","E","C","U","R"}, 
 										{"I","T","Y","A","B"},
 										{"D","F","G","H","K"},
 										{"L","M","N","O","P"},
 										{"Q","V","W","X","Z"}}
-	_ = playfair_matrix
 	cipher_text := "B MH AFC MUVY EOHPTCS, AFCSS TE QCSI NTYIMS TNA AFCSC. EMRBH XAA VAFR MIUCQPUH LMRLCCETOT FN HM AKUXAHK. OTA WANA OTXT FFU EISCWNAF HME BFU MCVA UGTOTRE. BM HYLF IFU UVTY ANE HBSEI QYOQM OUVSF AM EAFTE PYHYS XNSKE IFUSC."
 	//key := "SECURITY"
 	bigrams := []string{}
 	cipher_text = strings.ReplaceAll(cipher_text, " ", "")
 	cipher_text = strings.ReplaceAll(cipher_text, ".", "")
+	
+	// Bigram Generation
 	for i := 0; i < len(cipher_text); i += 2 {
 		temp := ""
 		if (i < len(cipher_text)-1) {
@@ -26,6 +32,16 @@ func main() {
 		}
 		bigrams = append(bigrams, temp)
 	}
+
+	// Location Mapping
+	m := make(map[string]loc)
+	for i := 0; i < 5; i++ {
+		for j := 0; j < 5; j++ {
+			m[playfair_matrix[i][j]] = loc {i, j}
+		}
+	}
+	
 	fmt.Println(cipher_text)
 	fmt.Println(bigrams)
+	fmt.Println(m["A"].x)
 }
