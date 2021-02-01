64f1e426b0a2223c1aec96e6f79b6d38fd441f5a
rohanblueboybaijal <rohanbaijal2702@gmail.com>
2021-02-01 21:49:48 +0530

    Create Playfair square

--- Level2/playfair_decryption.go
+++ Level2/playfair_decryption.go
@@ -3,6 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	var playfair_matrix = [2][3]string{{"a","b","c"}, {"d","e","f"}}
-	fmt.Println(playfair_matrix[0][0])
+	var playfair_matrix = [5][5]string{{"S","E","C","U","R"}, 
+										{"I","T","Y","A","B"},
+										{"D","F","G","H","K"},
+										{"L","M","N","O","P"},
+										{"Q","V","W","X","Z"}}
+	fmt.Println(playfair_matrix[3][4])
 }
