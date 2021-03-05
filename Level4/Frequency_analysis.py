0a80db58a703995f8828a768d112726c07a6d54b
Sarvesh Chandra <58358506+poseidon078@users.noreply.github.com>
2021-03-06 01:38:41 +0530

    Add files via upload

---
+++ Level4/Frequency_analysis.py
@@ -0,0 +1,32 @@
+file='keys.txt'
+cand_keys=[]
+for i in range(0,8):
+    cand_keys.append([])
+    
+for i in range(0,8):
+    for j in range(0,64):
+        cand_keys[i].append(0)
+
+with open(file) as f:
+    content=f.read().splitlines()
+    for k in range(len(content)//8):
+        for i in range(8):
+            # print(k)
+            for j in list(content[8*k+i].split(' ')):
+                
+                if j!='':
+                    a=int(j,2)
+
+                    cand_keys[i][a]+=1
+
+for i in range(8):
+    s=sum(cand_keys[i])/64
+    m=max(cand_keys[i])
+    f=-1
+    for j in range(64):
+        if cand_keys[i][j]==m:
+            f=j
+            break
+    if f>0:
+        print(f,m,s)
+
